b7a3a2167fc5833e2623a75c29e9b65ec4bfeada
venkatesh-jacke <venkateshjacke@gmail.com>
2024-01-13 18:08:04 +0530

    added ncomment for merge sort

--- src/Sorting/MergeSort_2.java
+++ src/Sorting/MergeSort_2.java
@@ -18,7 +18,7 @@ public class MergeSort_2 {
 
     static void mergeSort(int arr[], int start, int end) {
         if (start >= end) return;
-        int mid = start + (end - start) / 2;
+        int mid = start + (end - start) / 2; // to avoid overflow
         mergeSort(arr, start, mid);
         mergeSort(arr, mid + 1, end);
         merge(arr, start, mid, end);
@@ -26,7 +26,7 @@ public class MergeSort_2 {
     }
 
     static void merge(int arr[], int start, int mid, int end) {
-        int merged[] = new int[end - start + 1];
+        int merged[] = new int[end - start + 1];  // to maintain correct length
 
         int i = start;
         int j = mid+1;
