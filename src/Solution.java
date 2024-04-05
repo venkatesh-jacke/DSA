f628ad36fa33b492f5ba8221c9a8e112e7fd828f
venkatesh-jacke <venkateshjacke@gmail.com>
2024-04-05 22:56:04 +0530

    codeforce 05-04-2024

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,26 @@
-
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-      String[] s= new String[]{"ab", "c"};
-        System.out.println(String.join("",s));
+        int[] arr = new int[]{1, 2, 0, 0, 0, 3, 6};
+
+        System.out.println(Arrays.toString(moveZeros(arr)));
+
     }
 
+    static int[] moveZeros(int[] arr) {
+        int n = arr.length;
+        int c = 0;
 
-
-
+        for (int i = 0; i <n; i++) {
+            if (arr[i] != 0) {
+                arr[c++] = arr[i];
+            }
+        }
+        while(c<n){
+            arr[c++]=0;
+        }
+        return arr;
+    }
 }
