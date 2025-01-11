1630cd0208cfd35f1bc617f4f302e8288b15bfe7
venkatesh-jacke <venkateshjacke@gmail.com>
2025-01-11 11:16:28 +0530

    longestUniqueSubstr

---
+++ src/gfg/LongestUniqueSubstr.java
@@ -0,0 +1,29 @@
+package gfg;
+
+
+//https://www.geeksforgeeks.org/problems/longest-distinct-characters-in-string5848/1
+public class LongestUniqueSubstr {
+    public static void main(String[] args) {
+        String s = "abcdefabcbb";
+        System.out.println(longestUniqueSubstr(s));
+    }
+
+
+    static public int longestUniqueSubstr(String s) {
+        // code here
+        int ans = 0;
+
+        boolean[] vis = new boolean[26];
+        for (int l = 0, r = 0; r < s.length(); r++) {
+            char cur = s.charAt(r);
+            while (vis[cur - 'a']) {
+                vis[s.charAt(l) - 'a'] = false;
+                l++;
+            }
+            vis[cur - 'a'] = true;
+            ans = Math.max(ans, r - l + 1);
+        }
+        return ans;
+    }
+
+}
