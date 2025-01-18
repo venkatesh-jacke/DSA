26b0f1263b787678509901b1664ad7f9c8c9ab21
venkatesh-jacke <venkateshjacke@gmail.com>
2025-01-18 12:33:54 +0530

    https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1

---
+++ src/gfg/ReverseList.java
@@ -0,0 +1,28 @@
+package gfg;
+
+//https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1
+
+public class ReverseList {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        Node.PrintLL(reverseList(head));
+    }
+
+    static Node reverseList(Node head) {
+        Node prev = null;
+        Node cur = head;
+
+        while (cur != null) {
+            Node next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+
+}
